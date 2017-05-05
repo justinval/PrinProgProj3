cc2f63e6ba67f577046b8a784ee13ef1c6f36ee0
imjustinv <imjustinv@gmail.com>
2017-05-05 01:45:40 -0400

    updated #param

--- spell_t2_singleloop.c
+++ spell_t2_singleloop.c
@@ -56,8 +56,8 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	#pragma omp parallel for schedule(runtime) private(i, hash)
-	
+	#pragma omp parallel for schedule(runtime) private(j, hash)
+
 	for (i = 0; i < wl_size; i++) {
 		for (j = 0; j < num_hf; j++) {
 			hash = hf[j] (get_word(wl, i));
